a9ee836a7bede2086bf98c488194a0e87624b512
Jason Etcovitch <jasonetco@github.com>
2019-09-04 13:53:01 -0400

    Try stuff

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const SmeeClient = require('smee-client')
+const app = express()
+
+const PORT = 58000
+const channel = process.env.GITHUB_REPOSITORY.replace('/', '-') + '-' + process.env.GITHUB_REF
+
+const smee = new SmeeClient({
+  source: `https://smee.io/${channel}`,
+  target: `http://localhost:${PORT}/events`,
+  logger: console
+})
+
+const events = smee.start()
+
+events.onmessage(evt => {
+  console.log(evt)
+})
+
+app.get('/hello', (req, res) => res.send('Hello!'))
+app.post('/events', (req, res) => {
+  console.log(req.body)
+  res.sendStatus(200)
+})
+
+app.listen(PORT)
